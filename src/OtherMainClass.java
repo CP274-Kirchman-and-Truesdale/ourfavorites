1bc717fada173909efab9b1b467f6d6275c286e8
Jack Truesdale <jtrues13@gmail.com>
2017-05-09 10:50:07 -0600

    Add printThisIsANewBranch method to this-is-a-new-branch branch.

--- src/OtherMainClass.java
+++ src/OtherMainClass.java
@@ -3,6 +3,11 @@ public class OtherMainClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is the other main class!");
+		printThisIsANewBranch();
 	}
 
+	public static void printThisIsANewBranch(){
+		System.out.println("This is a new branch!");
+	}
+	
 }
