2ebbfa58d9b5f565cd6bb6f3aa18bd1f472fd9e0
Jack Truesdale <jtrues13@gmail.com>
2017-05-09 10:17:25 -0600

    Add MainClass and OtherMainClass. Update .gitignore.

---
+++ src/MainClass.java
@@ -0,0 +1,10 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("THIS IS THE MAIN CLASS!");
+		System.out.println("THIS IS ALSO THE MAIN CLASS!");
+		System.out.println("THIS TOO IS THE MAIN CLASS!");
+	}
+
+}
---
+++ src/OtherMainClass.java
@@ -0,0 +1,8 @@
+
+public class OtherMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the other main class!");
+	}
+
+}
